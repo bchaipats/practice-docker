ee88f80a891f7fb39a0afbaadfc69a71f3fe6e54
bchaipats <bchaipats@gmail.com>
2022-07-10 15:19:33 +0700

    bind mounts for development baby

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add It Fast!'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
